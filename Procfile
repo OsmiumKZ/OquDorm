aa1aa1454fd66dadc21491da083867c733cee169
FromSi <fromsi665@gmail.com>
2018-12-20 18:04:20 +0600

    Added CODE

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ./build/install/oqu-dorm/bin/oqu-dorm
---
+++ src/main/java/kz/osmium/Main.java
@@ -0,0 +1,75 @@
+package kz.osmium;
+
+import static spark.Spark.*;
+
+public class Main {
+
+    /**
+     * Запускает WebSocket
+     * Port и запросы настроенны именно в этом методе.
+     */
+    public static void main(String[] args) {
+        /* Изменяет port. */
+        port(getHerokuAssignedPort());
+
+        /* Конфигурация WebSocket */
+        config();
+
+        /* Разрешаю лок. серверу доступ к API */
+        preferences();
+
+        get("/", ((request, response) -> "Hello! D:"));
+    }
+
+    /**
+     * Конфигурация WebSocket
+     */
+    private static void config() {
+
+        after("/api/*", (req, res) -> res.type("application/json"));
+    }
+
+    /**
+     * Настройка сервера "Access-Control-Allow-Origin"
+     */
+    private static void preferences() {
+
+        options("/*",
+                (request, response) -> {
+
+                    String accessControlRequestHeaders = request
+                            .headers("Access-Control-Request-Headers");
+                    if (accessControlRequestHeaders != null) {
+                        response.header("Access-Control-Allow-Headers",
+                                accessControlRequestHeaders);
+                    }
+
+                    String accessControlRequestMethod = request
+                            .headers("Access-Control-Request-Method");
+                    if (accessControlRequestMethod != null) {
+                        response.header("Access-Control-Allow-Methods",
+                                accessControlRequestMethod);
+                    }
+
+                    return "OK";
+                });
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+    }
+
+    /**
+     * Настройка port'а
+     * С heroku.com приходит ответ зарег. порта для WebSocket'а
+     *
+     * @return если сервер запущен на стороне heroku.com, то будет использован port от heroku.com.
+     */
+    private static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+
+        return 4567;
+    }
+}
